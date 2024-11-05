10e949d625242f47b1d3a27eaacdc04153dd90b3
xyrel08 <xyrelgenio08@gmail.com>
2024-11-05 11:42:51 +0800

    initial commit

---
+++ GenioReact_Portfolio/src/App.jsx
@@ -0,0 +1,14 @@
+
+import styles from './App.module.css'
+import { Navbar } from './components/navbar'
+function App() {
+  
+
+  return (
+    <div className={styles.App}>
+      <Navbar></Navbar>
+    </div>
+  )
+}
+
+export default App
