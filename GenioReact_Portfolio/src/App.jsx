d29fcf5be46f582412d790c72f1404081a271616
xyrel08 <xyrelgenio08@gmail.com>
2024-11-06 21:52:30 +0800

    finished navbar

--- GenioReact_Portfolio/src/App.jsx
+++ GenioReact_Portfolio/src/App.jsx
@@ -1,12 +1,14 @@
 
 import styles from './App.module.css'
 import { Navbar } from './components/navbar'
+import { Hero } from './components/Hero/Hero'
 function App() {
   
 
   return (
     <div className={styles.App}>
       <Navbar></Navbar>
+      <Hero></Hero>
     </div>
   )
 }
---
+++ GenioReact_Portfolio/src/components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Hero = () => {
+  return (
+    <section>
+        <div>
+            <h1>
+            Hello I'm Xyrel Genio, an aspiring software developer.    
+            </h1>
+        </div>
+    </section>
+  )
+}
