eb44dc4e57e1e7d18966eee11a5469c432c2976e
xyrel08 <xyrelgenio08@gmail.com>
2024-11-08 23:38:12 +0800

    new

--- GenioReact_Portfolio/src/components/Hero/Hero.jsx
+++ GenioReact_Portfolio/src/components/Hero/Hero.jsx
@@ -7,6 +7,7 @@ export const Hero = () => {
             <h1>
             Hello I'm Xyrel Genio, an aspiring software developer.This is my portfolio showcasing my projects and skills and experience.    
             </h1>
+            <h2>Hello</h2>
         </div>
     </section>
   )
