efccce8fdea4de4c6bffc097446c6fefcf1532bb
xyrel08 <xyrelgenio08@gmail.com>
2024-11-10 22:35:10 +0800

    UPDATE hero section

--- GenioReact_Portfolio/src/components/Hero/Hero.jsx
+++ GenioReact_Portfolio/src/components/Hero/Hero.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
+import styles from './Hero.module.css'
+import {getImageUrl} from '../../utils'
 
 export const Hero = () => {
   return (
-    <section>
-        <div>
-            <h1>
-            Hello I'm Xyrel Genio, an aspiring software developer.This is my portfolio showcasing my projects and skills and experience.    
+    <section className={styles.container}>
+        <div className={styles.content}>
+            <h1 className={styles.title}>
+            Hello I'm Xyrel Genio!    
             </h1>
-            <h2>Hello</h2>
+            <p className={styles.desc}>
+              I am an aspiring software developer. This is my portfolio showcasing my projects and skills and experience.
+            </p>
+            <a href="mailto:xyrelgenio08@gmail.com" className={styles.contact}>Contact Me</a>
+            <img src={getImageUrl('hero/heroImage.png')}alt='Image of Me' className={styles.image}></img>
+        </div>
+        <div className={styles.top}>
+
+        </div>
+        <div className={styles.bottom}>
+
         </div>
     </section>
   )
